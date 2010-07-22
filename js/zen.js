aa14496852bfe079665ccaaa76cfe1965103c220
Long Ouyang <longouyang@gmail.com>
2010-07-22 15:16:41 -0400

    Removed XHR method, added replaceHtml()

--- js/zen.js
+++ js/zen.js
@@ -471,16 +471,15 @@ function eraseCookie(name) {
 	createCookie(name,"",-1);
 }
 
-
-if (typeof XMLHttpRequest == "undefined") {
-	XMLHttpRequest = function () {
-	try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
-		catch (e) {}
-	try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
-		catch (e) {}
-	try { return new ActiveXObject("Msxml2.XMLHTTP"); }
-		catch (e) {}
-	//Microsoft.XMLHTTP points to Msxml2.XMLHTTP.3.0 and is redundant
-	throw new Error("This browser does not support XMLHttpRequest.");
-	};
-}
+/* From http://blog.stevenlevithan.com/archives/faster-than-innerhtml */
+function replaceHtml(el, html) {
+	var oldEl = typeof el === "string" ? document.getElementById(el) : el;
+	/*@cc_on // Pure innerHTML is slightly faster in IE
+		oldEl.innerHTML = html;
+		return oldEl;
+	@*/
+	var newEl = oldEl.cloneNode(false);
+	newEl.innerHTML = html;
+	oldEl.parentNode.replaceChild(newEl, oldEl);
+	return newEl;
+};
